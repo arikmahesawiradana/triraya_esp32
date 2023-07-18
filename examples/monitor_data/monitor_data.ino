b827a2518297d0d32699d93d815a25d123db3b08
Arik Mahesa Wiradana <84776510+arikmahesawiradana@users.noreply.github.com>
2023-07-18 21:10:10 +0700

    Update monitor_data.ino

--- examples/monitor_data/monitor_data.ino
+++ examples/monitor_data/monitor_data.ino
@@ -1,11 +1,9 @@
 #include <WiFi.h>
 #include <WiFiAP.h>
 #include <triraya.h>
-#define adc_pin 15
 
 char *ssid = "yourAP";
 char *password = "yourPassword";
-char* param1 = "param1";
 
 //initial for pwm's frequency and resolution
 Triraya myraya(1000, 8); //1kHz freq and 8 bit resolution
@@ -13,7 +11,7 @@ Triraya myraya(1000, 8); //1kHz freq and 8 bit resolution
 void setup() {
   Serial.begin(115200);
   myraya.init(ssid, password); //activate the AP SSID and Password
-  myraya.monitor_data(send_data); //add analog pin (sensor) at pin 25, 26, or 27
+  myraya.monitor_data(); //add analog pin (sensor) at pin 25, 26, or 27
 }
 
 void loop() { 
