f5cab99db68b7b4e78185e6d5a9cc625f9105c8b
Arik Mahesa Wiradana <84776510+arikmahesawiradana@users.noreply.github.com>
2023-07-18 21:09:32 +0700

    Update monitor_data.ino

--- examples/monitor_data/monitor_data.ino
+++ examples/monitor_data/monitor_data.ino
@@ -13,10 +13,9 @@ Triraya myraya(1000, 8); //1kHz freq and 8 bit resolution
 void setup() {
   Serial.begin(115200);
   myraya.init(ssid, password); //activate the AP SSID and Password
+  myraya.monitor_data(send_data); //add analog pin (sensor) at pin 25, 26, or 27
 }
 
 void loop() { 
-  int my_adc = analogRead(adc_pin);
-  char* send_data = param1 + my_adc;
-  myraya.monitor_data(send_data);
+  
 }
